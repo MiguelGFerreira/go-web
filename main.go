4ec63704a61bdd4028d4a34451a9a0e8dd7be0c6
miguel <miguel.gfe@outlook.com>
2024-06-04 17:40:20 -0300

    Adds connection to postgres database

--- main.go
+++ main.go
@@ -1,11 +1,17 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"html/template"
 	"net/http"
+	"os"
+
+	_ "github.com/lib/pq"
 )
 
 type Produto struct {
+	Id         int
 	Nome       string
 	Descricao  string
 	Preco      float64
@@ -20,11 +26,46 @@ func main() {
 }
 
 func index(w http.ResponseWriter, r *http.Request) {
-	produtos := []Produto{
-		{Nome: "Camiseta", Descricao: "Azul, bem bonita", Preco: 39, Quantidade: 5},
-		{"Tênis", "Confortável", 89, 3},
-		{"Fone", "Muito bom", 59, 2},
+	db := connectDatabase()
+
+	selectAll, err := db.Query("select * from produtos")
+
+	if err != nil {
+		panic(err.Error())
+	}
+
+	p := Produto{}
+	produtos := []Produto{}
+
+	for selectAll.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err := selectAll.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		p.Nome = nome
+		p.Descricao = descricao
+		p.Preco = preco
+		p.Quantidade = quantidade
+
+		produtos = append(produtos, p)
 	}
 
 	temp.ExecuteTemplate(w, "Index", produtos)
+	defer db.Close()
+}
+
+func connectDatabase() *sql.DB {
+	DB_USER := os.Getenv("DB_USER")
+	DB_PASS := os.Getenv("DB_PASS")
+	connectionString := fmt.Sprintf("user=%s dbname=loja password=%s host=localhost sslmode=disable", DB_USER, DB_PASS)
+	db, err := sql.Open("postgres", connectionString)
+	if err != nil {
+		panic(err.Error())
+	}
+	return db
 }
