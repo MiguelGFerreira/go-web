52ac970a0559737d94ccb8dff6dbac439f1f4e00
miguel <miguel.gfe@outlook.com>
2024-06-06 10:00:30 -0300

    feat: adds new products page

--- controllers/products.go
+++ controllers/products.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"net/http"
 	"pkg/models"
+	"strconv"
 	"text/template"
 )
 
@@ -11,4 +12,30 @@ var temp = template.Must(template.ParseGlob("templates/*.html"))
 func Index(w http.ResponseWriter, r *http.Request) {
 	allProducts := models.GetAllProducts()
 	temp.ExecuteTemplate(w, "Index", allProducts)
+}
+
+func New(w http.ResponseWriter, r *http.Request) {
+	temp.ExecuteTemplate(w, "New", nil)
+}
+
+func Insert(w http.ResponseWriter, r *http.Request) {
+	if r.Method == "POST" {
+		name := r.FormValue("name")
+		description := r.FormValue("description")
+		price := r.FormValue("price")
+		quantity := r.FormValue("quantity")
+
+		convertedPrice, err := strconv.ParseFloat(price, 64)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		convertedQuantity, err := strconv.Atoi(quantity)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		models.CreateNewProduct(name, description, convertedPrice, convertedQuantity)
+	}
+	http.Redirect(w, r, "/", 301)
 }
